ccb23a9eff29303e55a81cf71f25e1a76d8a6a3b
Jonah Lovitz <129468112+jlovitz19@users.noreply.github.com>
2025-04-06 22:33:51 -0400

    Create sim_circular_orbit.m

---
+++ sim_circular_orbit.m
@@ -0,0 +1,23 @@
+function state = sim_circular_orbit(h)
+    % Simulates a circular orbit
+    %
+    % Parameters:
+    % h: altitude of circular orbit above Earth's atmosphere
+    %
+    % Outputs:
+    % state: state vector consisting of the x and y coordinates of the
+    % circular orbit
+
+
+    % radius of earth
+    R_E = 6378;
+
+    r = R_E + h;
+    
+    theta = 0:0.0001:2*pi;
+
+    x = r*cos(theta);
+    y = r*sin(theta);
+
+    state = [x; y];
+end
