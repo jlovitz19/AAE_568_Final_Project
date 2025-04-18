52a37b3139bd113e2a702cc3b3e38fe7a97f8772
Jacob <ajacobwolf@gmail.com>
2025-04-18 13:31:05 -0400

    fixed state vector, removed evil simplify()

--- find_hamiltonian.m
+++ find_hamiltonian.m
@@ -7,9 +7,7 @@ syms rho theta phi drho dtheta dphi ddrho ddtheta ddphi omega_sat_1 omega_sat_2 
 syms q_sat_1 q_sat_2 q_sat_3 q_sat_4 q_ant_1 q_ant_2 q_ant_3 q_ant_4 e_theta e_phi theta_max phi_max error_weight real
 syms lambda_1 lambda_2 lambda_3 lambda_4 lambda_5 lambda_6 lambda_7 lambda_8 lambda_9 lambda_10 lambda_11 lambda_12 lambda_13 lambda_14 lambda_15 lambda_16 lambda_17 lambda_18 lambda_19 lambda_20 lambda_21 lambda_22
 syms mu_1 mu_2 mu_3 mu_4 u_1 u_2 u_3 u_4 u_5
-syms I_ant_11 I_ant_12 I_ant_13 I_ant_21 I_ant_22 I_ant_23 I_ant_31 I_ant_32 I_ant_33
-%syms I_sat_11 I_sat_12 I_sat_13 I_sat_21 I_sat_22 I_sat_23 I_sat_31 I_sat_32 I_sat_33
-syms l m_sat m_ant r_cg_1 r_cg_2 r_cg_3 t
+syms l m_sat m_ant r_cg_1 r_cg_2 r_cg_3 r_max d_max t_max m_ant
 
 % form state
 w_sat = [omega_sat_1, omega_sat_2, omega_sat_3].';
@@ -20,7 +18,9 @@ err = [e_phi, e_theta].';
 
 
 %% ORDER IS RHO, PHI, THETA %%
-x = [rho; phi; theta; drho; dphi; dtheta; w_sat; w_ant; q_LOS2SAT; q_SAT2ANT; err]; 
+
+% erm are dphi dro and dtheta supposed ?
+x = [rho; phi; theta; drho; rho*dphi; rho*dtheta*sin(phi); w_sat; w_ant; q_LOS2SAT; q_SAT2ANT; err]; 
 
 % form control
 u = [u_1 u_2 u_3 u_4 u_5].';
@@ -32,11 +32,12 @@ n = length(x);
 Q = sym(zeros(n)); Q(n-1:n, n-1:n) = diag(error_weight*sym(ones(2,1)));
 
 % get MOI
-I_ant = [I_ant_11 I_ant_12 I_ant_13; I_ant_21 I_ant_22 I_ant_23; I_ant_31 I_ant_32 I_ant_33]; % <--- needs to be a function based on geometry
+[I_ant, r_cg_ant] = ant_inertia(r_max, d_max, t_max, m_ant);
 I_sat = PMOI_rectangular_prism(m_sat, [l;l;l]); % m is mass of satellite l is length (its a cubesat, probably 50kg later)
 
 DCM = quat2dcm(q_SAT2ANT); % right quaternion?
-I_total = I_sat + DCM*parallel_axis_thm([r_cg_1; r_cg_2; r_cg_3], m_ant, I_ant)*DCM.'; % <-- need parellel axis theorem
+r_cg = r_cg_ant  + ones(3,1) * l / 2;
+I_total = I_sat + DCM*parallel_axis_thm(r_cg, m_ant, I_ant)*DCM.'; % <-- need parellel axis theorem
 
 % form R
 %R = diag([I_total, I_total, I_total, I_ant, I_ant, I_ant]);
@@ -111,12 +112,9 @@ dx = [
 
 % hamiltonian
 H = x.'*Q*x + u.'*R*u + lambda.'*dx + mu.'*c;
-latex(simplify(H))
 
 
-
-
-
+%diff(H, [u1, u2, u3, u4, u5])
 % pitch is theta, yaw is phi (this is needed for later, but cant use in
 % syms-land)
 %[e_theta, e_phi, ~] = dcm2angle(DCM_LOS2ANT, 'ZYX');
